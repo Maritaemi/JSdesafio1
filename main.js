79e3a26e518ba68febae948f29835950c3e4c4cf
Maritaemi <marianaemiliozzi@gmail.com>
2021-09-24 17:42:39 +0200

    Primeros dos desafios de JavaScrip

---
+++ main.js
@@ -0,0 +1,13 @@
+//Ingresar nombre, apellido y edad del estudiante//
+
+let nombre   = prompt('Introduzca su nombre');
+let apellido = prompt('Introduzca su apellido');
+let edad     = Number(prompt('Ingrese su edad'));
+
+alert(`Hola ${nombre} ${apellido}, tu edad es ${edad}`)
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+
